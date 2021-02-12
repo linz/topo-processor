f5b5d9c3d789af2b35be2770949bde1a962ef519
Megan <mdavidson@linz.govt.nz>
2021-02-12 14:15:12 +1300

    feat: ability to write stac object to json file

--- topo_processor/util/__init__.py
+++ topo_processor/util/__init__.py
@@ -1,3 +1,4 @@
 from .checksum import multihash_as_hex
+from .json_writer import write_stac_object
 from .tiff import is_tiff
 from .time import time_in_ms
---
+++ topo_processor/util/json_writer.py
@@ -0,0 +1,23 @@
+from typing import Union
+
+import pystac as stac
+from linz_logger import get_log
+
+from topo_processor.metadata.collection import Collection
+from topo_processor.metadata.item import Item
+from topo_processor.util.time import time_in_ms
+
+
+async def write_stac_object(stac_object: Union[Item, Collection], destination: str):
+    start_time = time_in_ms()
+    if isinstance(stac_object, Item):
+        obj = stac_object.stac_item
+    else:
+        obj = stac_object.stac_collection
+
+    stac.write_file(obj=obj, include_self_link=True, dest_href=destination)
+    get_log().debug(
+        "STAC object written to file",
+        duration=time_in_ms() - start_time,
+        destination=destination,
+    )
