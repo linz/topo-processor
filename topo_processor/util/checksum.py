8cae8e95ab581d455c9ce5454c9f0cbdae74dc97
Ben-Cheng <BCheng@linz.govt.nz>
2021-02-12 15:22:29 +1300

    feat: make checksum async

--- topo_processor/util/checksum.py
+++ topo_processor/util/checksum.py
@@ -1,5 +1,5 @@
 import hashlib
-
+import aiofiles
 import multihash
 
 CHUNK_SIZE = 1024 * 1024  # 1MB
@@ -7,7 +7,7 @@ CHUNK_SIZE = 1024 * 1024  # 1MB
 
 async def multihash_as_hex(file_path: str) -> str:
     file_hash = hashlib.sha256()
-    with open(file_path, "rb") as file:
-        while chunk := file.read(CHUNK_SIZE):
+    async with aiofiles.open(file_path, 'rb') as file:
+        while chunk := await file.read(CHUNK_SIZE):
             file_hash.update(chunk)
     return multihash.to_hex_string(multihash.encode(file_hash.digest(), "sha2-256"))
