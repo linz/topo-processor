5e1fc40df085e3867b338353da7f17762f89e659
Ben-Cheng <BCheng@linz.govt.nz>
2021-03-24 15:40:57 +1300

    test: replace asyncio.run with pytest.mark.asyncio

--- topo_processor/cog/tests/command_test.py
+++ topo_processor/cog/tests/command_test.py
@@ -1,20 +1,22 @@
-import asyncio
+import pytest
 
 from topo_processor.cog.command import Command
 
 
-def test_hello_world_local():
+@pytest.mark.asyncio
+async def test_hello_world_local():
     cmd = Command("echo")
     cmd.arg("Hello World Local!!!")
-    return_code, stdout, stderr = asyncio.run(cmd.run())
+    return_code, stdout, stderr = await cmd.run()
     assert stdout == "Hello World Local!!!\n"
     assert stderr == ""
     assert return_code == 0
 
 
-def test_hello_world_docker():
+@pytest.mark.asyncio
+async def test_hello_world_docker():
     cmd = Command("/bin/echo", {"container": "busybox", "tag": "latest"})
     cmd.arg("Hello World Docker!!!")
-    return_code, stdout, stderr = asyncio.run(cmd.run())
+    return_code, stdout, stderr = await cmd.run()
     assert stdout == "Hello World Docker!!!\n"
     assert return_code == 0
