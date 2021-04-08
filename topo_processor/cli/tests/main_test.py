8ccd57be6642d4358b57580bc8ddbf4bb2090858
Ben-Cheng <BCheng@linz.govt.nz>
2021-04-09 11:17:40 +1200

    fix: improve test data names

--- topo_processor/cli/tests/main_test.py
+++ topo_processor/cli/tests/main_test.py
@@ -23,7 +23,7 @@ def setup():
 
 @pytest.mark.asyncio
 async def test_upload_local(setup):
-    source_dir = os.path.abspath(os.path.join(os.getcwd(), "test_data", "tiffs", "399"))
+    source_dir = os.path.abspath(os.path.join(os.getcwd(), "test_data", "tiffs", "SURVEY_1"))
     target = setup
     await process_directory(source_dir)
     try:
@@ -33,21 +33,23 @@ async def test_upload_local(setup):
         for collection in collection_store.values():
             collection.delete_temp_dir()
 
-    assert os.path.isfile(os.path.join(target, "398", "72352.json"))
-    assert os.path.isfile(os.path.join(target, "398", "72352.tiff"))
-    assert os.path.isfile(os.path.join(target, "398", "collection.json"))
+    assert os.path.isfile(os.path.join(target, "SURVEY_3", "72352.json"))
+    assert os.path.isfile(os.path.join(target, "SURVEY_3", "72352.tiff"))
+    assert os.path.isfile(os.path.join(target, "SURVEY_3", "collection.json"))
 
-    assert os.path.isfile(os.path.join(target, "399", "72360.json"))
-    assert os.path.isfile(os.path.join(target, "399", "72360.tiff"))
-    assert os.path.isfile(os.path.join(target, "399", "collection.json"))
+    assert os.path.isfile(os.path.join(target, "SURVEY_1", "72360.json"))
+    assert os.path.isfile(os.path.join(target, "SURVEY_1", "72360.tiff"))
+    assert os.path.isfile(os.path.join(target, "SURVEY_1", "collection.json"))
 
-    with open(os.path.join(target, "398", "72352.json")) as item_json_file:
+    with open(os.path.join(target, "SURVEY_3", "72352.json")) as item_json_file:
         item_metadata = json.load(item_json_file)
-    assert item_metadata["properties"]["linz:survey"] == "398"
+    assert item_metadata["properties"]["linz:survey"] == "SURVEY_3"
     assert item_metadata["properties"]["linz:sufi"] == "72352"
     assert item_metadata["id"] == item_metadata["properties"]["linz:sufi"]
     assert (
         item_metadata["assets"]["image/tiff; application=geotiff; profile=cloud-optimized"]["file:checksum"]
         == "1220b8f2e22e2d8059ec7c4b327bb695f6a8dc55bdb5f5865b0d2628867f16dca840"
     )
-    assert (item_metadata["assets"]["image/tiff; application=geotiff; profile=cloud-optimized"]["href"]) == "./398/72352.tiff"
+    assert (
+        item_metadata["assets"]["image/tiff; application=geotiff; profile=cloud-optimized"]["href"]
+    ) == "./SURVEY_3/72352.tiff"
