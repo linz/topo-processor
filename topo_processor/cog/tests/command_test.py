0d591595c9ff887dd3a46b1535af21a6447f7472
Megan <mdavidson@linz.govt.nz>
2021-04-13 13:39:27 +1200

    test: add mock to docker test
    (and fix an import)

--- topo_processor/cog/tests/command_test.py
+++ topo_processor/cog/tests/command_test.py
@@ -14,9 +14,10 @@ async def test_hello_world_local():
 
 
 @pytest.mark.asyncio
-async def test_hello_world_docker():
+async def test_hello_world_docker(mocker):
     cmd = Command("/bin/echo", {"container": "busybox", "tag": "latest"})
     cmd.arg("Hello World Docker!!!")
-    return_code, stdout, stderr = await cmd.run()
+    mocker.patch("topo_processor.cog.execution.ExecutionLocal.run", return_value=[0, "Hello World Docker!!!\n", ""])
+    return_code, stdout, _ = await cmd.run()
     assert stdout == "Hello World Docker!!!\n"
     assert return_code == 0
