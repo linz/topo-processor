15055f8955d9f9334fd62b6b01fd353719b4eada
Ben-Cheng <BCheng@linz.govt.nz>
2021-05-11 10:53:23 +1200

    fix: fix test assert is s3 instance

--- topo_processor/file_system/get_fs.py
+++ topo_processor/file_system/get_fs.py
@@ -1,9 +1,8 @@
-import fsspec
 from fsspec.implementations.local import LocalFileSystem
 from s3fs import S3FileSystem
 
 
 def get_fs(path: str):
     if path.startswith("s3://"):
-        return S3FileSystem
+        return S3FileSystem()
     return LocalFileSystem(auto_mkdir="True")
