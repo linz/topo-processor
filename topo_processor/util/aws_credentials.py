74d1893bc6991b52c64a18adf5acad82ffe9cd19
Megan <mdavidson@linz.govt.nz>
2021-05-14 15:55:04 +1200

    fix: try get credentials from role before profile

--- topo_processor/util/aws_credentials.py
+++ topo_processor/util/aws_credentials.py
@@ -3,11 +3,32 @@ import os
 import boto3
 
 credentials = None
+role_dict = {}
 
 
-def get_credentials():
+def get_credentials(bucket_name: str):
     global credentials
-    if not credentials:
-        session = boto3.Session(profile_name=os.getenv("AWS_PROFILE"))
-        credentials = session.get_credentials()
+    if credentials:
+        return credentials
+
+    session = boto3.Session(profile_name=os.getenv("AWS_PROFILE"))
+
+    if bucket_name in role_dict:
+        if not role_dict[bucket_name]["client"]:
+            client = boto3.client()
+            client.assume_role(RoleArn=role_dict[bucket_name]["role"])
+            role_dict[bucket_name]["client"] = client
+        cred = role_dict[bucket_name]["client"].get("Credentials")
+        credentials = {
+            "access_key": cred.get("AccessKeyId"),
+            "secret_key": cred.get("SecretAccessKey"),
+            "token": cred.get("SessionToken"),
+        }
+    else:
+        session_credentials = session.get_credentials()
+        credentials = {
+            "access_key": session_credentials.access_key,
+            "secret_key": session_credentials.secret_key,
+            "token": session_credentials.token,
+        }
     return credentials
