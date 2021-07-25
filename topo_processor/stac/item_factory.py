f01542ceec2c479bc723eb4a270518ca45e956a0
Megan Davidson <33814653+MDavidson17@users.noreply.github.com>
2021-07-26 11:44:19 +1200

    fix: recursive file scanning (#277)
    * fix: recursively scan files
    
    * chore: update codeowners
    
    * fix: get full path of file programmatically
    
    as opposed to hardcoding it based on file system
    
    * tests: add unit test
    
    * fix: get full path with a more robust method

--- topo_processor/stac/item_factory.py
+++ topo_processor/stac/item_factory.py
@@ -1,9 +1,11 @@
 import asyncio
+import os
 
 from linz_logger import get_log
 
 from topo_processor.data.data_transformers import data_transformer_repo
 from topo_processor.file_system.get_fs import get_fs, is_s3_path
+from topo_processor.file_system.get_path_with_protocol import get_path_with_protocol
 from topo_processor.metadata.metadata_loaders import metadata_loader_repo
 from topo_processor.metadata.metadata_validators import metadata_validator_repo
 from topo_processor.stac.store import get_asset, item_store
@@ -21,12 +23,13 @@ async def process_directory(source_dir: str) -> None:
 async def _create_assets(source_dir: str) -> None:
     assets_to_process = []
     fs = get_fs(source_dir)
-    for file_ in fs.ls(source_dir):
-        if is_s3_path(source_dir):
-            asset = get_asset(f"s3://{file_}")
-        else:
-            asset = get_asset(file_)
-        assets_to_process.append(metadata_loader_repo.load_metadata(asset))
+    for (path, _, files) in fs.walk(source_dir):
+        if not files:
+            continue
+        for file_ in files:
+            asset_path = get_path_with_protocol(source_dir, fs, path)
+            asset = get_asset(f"{asset_path}/{file_}")
+            assets_to_process.append(metadata_loader_repo.load_metadata(asset))
     await asyncio.gather(*assets_to_process)
 
 
