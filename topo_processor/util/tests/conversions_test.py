1e4b717bb6f929c5c38cdbfd18802778da65f668
paulfouquet <86932794+paulfouquet@users.noreply.github.com>
2021-11-02 08:42:38 +1300

    feat: set temporal extent (TDE-148) (#393)
    * feat: determine STAC Temporal Extent for Historical Imagery
    
    * fix: set item datetime as None by default and item to be not valid if invalid or empty date as it is a required property for STAC
    
    * chore: sort imports
    
    * chore: formatting
    
    * fix: merging issue
    
    * fix: 	- improve performances to get min and max date for get_temporal_extent
    	- add unit tests
    	- remove mypy comment

---
+++ topo_processor/util/tests/conversions_test.py
@@ -0,0 +1,13 @@
+import pytest
+
+from topo_processor.util import nzt_datetime_to_utc_datetime
+
+
+def test_nzt_datetime_to_utc_datetime_daylight_saving_on():
+    utc_date = nzt_datetime_to_utc_datetime("1988-01-11T00:00:00.000")
+    assert utc_date.isoformat() == "1988-01-10T11:00:00+00:00"
+
+
+def test_nzt_datetime_to_utc_datetime_daylight_saving_off():
+    utc_date = nzt_datetime_to_utc_datetime("1988-07-11T00:00:00.000")
+    assert utc_date.isoformat() == "1988-07-10T12:00:00+00:00"
