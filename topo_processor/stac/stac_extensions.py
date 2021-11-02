84aa4255799a32465bf8c29f48a877d197a25435
Megan Davidson <mdavidson@linz.govt.nz>
2021-11-02 16:01:00 +1300

    feat: update extension links

--- topo_processor/stac/stac_extensions.py
+++ topo_processor/stac/stac_extensions.py
@@ -2,9 +2,11 @@ from enum import Enum
 
 
 class HistoricalStacExtensions(Enum):
-    aerial_photo = "https://linz.github.io/stac/v0.0.7/aerial-photo/schema.json"
-    camera = "https://linz.github.io/stac/v0.0.2/camera/schema.json"
+    aerial_photo = "https://linz.github.io/stac/v0.0.10/aerial-photo/schema.json"
+    camera = "https://linz.github.io/stac/v0.0.10/camera/schema.json"
     projection = "https://stac-extensions.github.io/projection/v1.0.0/schema.json"
     file = "https://stac-extensions.github.io/file/v2.0.0/schema.json"
-    film = "https://linz.github.io/stac/v0.0.7/film/schema.json"
-    scanning = "https://linz.github.io/stac/v0.0.6/scanning/schema.json"
+    film = "https://linz.github.io/stac/v0.0.10/film/schema.json"
+    scanning = "https://linz.github.io/stac/v0.0.10/scanning/schema.json"
+    eo = "https://stac-extensions.github.io/eo/v1.0.0/schema.json"
+    historical_imagery = "https://linz.github.io/stac/v0.0.10/historical-imagery/schema.json"
