357ce631e115e13aaef30f3f13f4ee8328ddd780
Alice Fage <afage@linz.govt.nz>
2021-11-05 09:40:37 +1300

    fix: convert scanned date to UTC (TDE-187) (#406)
    * fix: scanned date to utc, tests, updated example spreadsheet for more quarter dates
    
    * fix: function name and typing
    
    * test: unit tests
    
    * fix: use f-string for quarterdate conversion

--- topo_processor/util/tests/conversions_test.py
+++ topo_processor/util/tests/conversions_test.py
@@ -1,6 +1,6 @@
 import pytest
 
-from topo_processor.util import nzt_datetime_to_utc_datetime
+from topo_processor.util import nzt_datetime_to_utc_datetime, quarterdate_to_date_string
 
 
 def test_nzt_datetime_to_utc_datetime_daylight_saving_on():
@@ -11,3 +11,13 @@ def test_nzt_datetime_to_utc_datetime_daylight_saving_on():
 def test_nzt_datetime_to_utc_datetime_daylight_saving_off():
     utc_date = nzt_datetime_to_utc_datetime("1988-07-11T00:00:00.000")
     assert utc_date.isoformat() == "1988-07-10T12:00:00+00:00"
+
+
+def test_quarter_date_to_utc_correct_format():
+    utc_date_string = quarterdate_to_date_string("2020/Q1")
+    assert utc_date_string == "2019-12-31T11:00:00Z"
+
+
+def test_quarter_date_to_utc_incorrect_format():
+    returned_string = quarterdate_to_date_string("nzam_pilot")
+    assert returned_string == "nzam_pilot"
