f4e89bd109e07b465f0c9e68150e7b0bfdd26870
Alice Fage <afage@linz.govt.nz>
2021-11-08 10:09:30 +1300

    test: added mocking test for asset

--- topo_processor/stac/tests/asset_test.py
+++ topo_processor/stac/tests/asset_test.py
@@ -25,9 +25,19 @@ def test_asset_key_visual():
 
 
 def test_asset_key_no_content_type():
-    """validate value of asset object key for an unknown content type with an extension"""
+    """validate value of asset object key for an unknown content type with a file extension"""
     source_path = os.path.abspath(os.path.join(os.getcwd(), "test_data", "tiffs", "SURVEY_1", "CONTROL.tiff"))
     asset = Asset(source_path)
     asset.content_type = ""
     key = asset.get_key()
     assert key == ".tiff"
+
+
+def test_asset_key_no_content_type_no_file_ext(mocker):
+    """validate value of asset object key for an unknown content type with no file extension"""
+    source_path = os.path.abspath(os.path.join(os.getcwd(), "test_data", "tiffs", "SURVEY_1", "CONTROL.tiff"))
+    asset = Asset(source_path)
+    asset.content_type = ""
+    mocker.patch("topo_processor.stac.asset.Asset.file_ext", return_value="")
+    key = asset.get_key()
+    assert key == asset.get_checksum()
