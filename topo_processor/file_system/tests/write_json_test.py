ed81123600319d4f4175fb887a0e20b8d147df13
Paul Fouquet <pfouquet@linz.govt.nz>
2021-11-10 10:31:53 +1300

    tests: add write_json test

---
+++ topo_processor/file_system/tests/write_json_test.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+from tempfile import mkdtemp
+
+import pytest
+
+from topo_processor.file_system.write_json import write_json
+
+
+@pytest.fixture(autouse=True)
+def setup():
+    """
+    This function creates a temporary directory and deletes it after each test.
+    See following link for details:
+    https://docs.pytest.org/en/stable/fixture.html#yield-fixtures-recommended
+    """
+    target = mkdtemp()
+    yield target
+    shutil.rmtree(target)
+
+
+def test_write_json(setup):
+    my_dict = {"foo": "foo", "bar": 1}
+    target = setup + '/test.json'
+    write_json(my_dict, target)
+    assert os.path.isfile(target)
