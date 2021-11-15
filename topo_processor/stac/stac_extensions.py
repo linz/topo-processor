471feabd0665bcda2b7c7cfb8e678e812ec7ca8f
Alice Fage <afage@linz.govt.nz>
2021-11-15 16:34:15 +1300

    feat: add all stac_extensions

--- topo_processor/stac/stac_extensions.py
+++ topo_processor/stac/stac_extensions.py
@@ -10,3 +10,8 @@ class StacExtensions(Enum):
     scanning = "https://linz.github.io/stac/v0.0.10/scanning/schema.json"
     eo = "https://stac-extensions.github.io/eo/v1.0.0/schema.json"
     historical_imagery = "https://linz.github.io/stac/v0.0.10/historical-imagery/schema.json"
+    processing ="https://stac-extensions.github.io/processing/v1.0.0/schema.json"
+    timestamps = "https://stac-extensions.github.io/timestamps/v1.0.0/schema.json"
+    linz = "https://linz.github.io/stac/v0.0.10/linz/schema.json"
+    quality = "https://linz.github.io/stac/v0.0.10/quality/schema.json"
+    version = "https://stac-extensions.github.io/version/v1.0.0/schema.json"
