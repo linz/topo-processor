49c88f53ed1202c5ee761deb1f82505646013b50
Megan Davidson <mdavidson@linz.govt.nz>
2021-11-17 11:01:27 +1300

    feat: update extensions to use new urls

--- topo_processor/stac/stac_extensions.py
+++ topo_processor/stac/stac_extensions.py
@@ -1,12 +1,12 @@
 from enum import Enum
 
 
-class StacExtensions(Enum):
-    aerial_photo = "https://linz.github.io/stac/v0.0.10/aerial-photo/schema.json"
-    camera = "https://linz.github.io/stac/v0.0.10/camera/schema.json"
+class StacExtensions(str, Enum):
+    aerial_photo = "https://stac.linz.govt.nz/v0.0.11/aerial-photo/schema.json"
+    camera = "https://stac.linz.govt.nz/v0.0.11/camera/schema.json"
     projection = "https://stac-extensions.github.io/projection/v1.0.0/schema.json"
     file = "https://stac-extensions.github.io/file/v2.0.0/schema.json"
-    film = "https://linz.github.io/stac/v0.0.10/film/schema.json"
-    scanning = "https://linz.github.io/stac/v0.0.10/scanning/schema.json"
+    film = "https://stac.linz.govt.nz/v0.0.11/film/schema.json"
+    scanning = "https://stac.linz.govt.nz/v0.0.11/scanning/schema.json"
     eo = "https://stac-extensions.github.io/eo/v1.0.0/schema.json"
-    historical_imagery = "https://linz.github.io/stac/v0.0.10/historical-imagery/schema.json"
+    historical_imagery = "https://stac.linz.govt.nz/v0.0.11/historical-imagery/schema.json"
