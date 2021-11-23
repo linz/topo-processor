7b190a1a758cdab3afa5efd773f0d74023b7dbd5
Paul Fouquet <pfouquet@linz.govt.nz>
2021-11-23 15:16:23 +1300

    fix: remove hardcoded aws role

--- topo_processor/stac/tests/lds_cache_test.py
+++ topo_processor/stac/tests/lds_cache_test.py
@@ -1,17 +1,7 @@
-from typing import Dict
-
 import pystac
 import pytest
 
 from topo_processor.stac.lds_cache import LdsCache
-
-
-def test_get_layer():
-    # FIXME: Mock this
-    metadata_path: str = ""
-    lds_cache: LdsCache = LdsCache("linz-lds-cache", "arn:aws:iam::413910103162:role/internal-user-read")
-    metadata_path = lds_cache.get_layer("51002")
-    assert metadata_path == "input_data/51002_322142.csv"
 
 
 def test_get_last_version():
@@ -23,14 +13,6 @@ def test_get_last_version():
     collection.add_links((link_a, link_b, link_c, link_d))
 
     assert LdsCache.get_last_version(collection) == "100001"
-
-
-def test_get_collection():
-    # FIXME: Mock this
-    lds_cache: LdsCache = LdsCache("linz-lds-cache", "arn:aws:iam::413910103162:role/internal-user-read")
-    collection: pystac.Collection = lds_cache.get_collection("51002")
-
-    assert collection.id == "sc_01FHPJY0NYZ5T5C6Q5QWK61YB5"
 
 
 def test_get_metadata_file_path():
