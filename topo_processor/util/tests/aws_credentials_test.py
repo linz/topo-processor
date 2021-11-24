48ef28866c4fff91d71e97932e619ecce4e28f7f
Paul Fouquet <pfouquet@linz.govt.nz>
2021-11-25 11:39:40 +1300

    fix(tests): update unit test to match bucket_roles dict structure changes

--- topo_processor/util/tests/aws_credentials_test.py
+++ topo_processor/util/tests/aws_credentials_test.py
@@ -8,7 +8,7 @@ from topo_processor.util.aws_credentials import bucket_roles, load_roles
 def test_load_roles():
     role_config = json.loads('{"s3://example-bucket": {"roleArn": "arn:aws:iam::0123456789:role/example-read"}}')
     load_roles(role_config)
-    assert bucket_roles["example-bucket"]["role"] == "arn:aws:iam::0123456789:role/example-read"
+    assert bucket_roles["example-bucket"]["roleArn"] == "arn:aws:iam::0123456789:role/example-read"
 
 
 @pytest.mark.skip(reason="Need to mock AWS to implement this test")
