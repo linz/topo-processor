ee9348e7e109fea4059d64f7c28de2dbb067e290
Paul Fouquet <pfouquet@linz.govt.nz>
2021-11-29 11:33:43 +1300

    chore: add asset in the logs

--- topo_processor/metadata/metadata_loaders/metadata_loader_repo.py
+++ topo_processor/metadata/metadata_loaders/metadata_loader_repo.py
@@ -33,6 +33,7 @@ class MetadataLoaderRepository:
                 get_log().debug(
                     "Metadata Loaded",
                     loader=loader.name,
+                    asset=asset,
                     duration=time_in_ms() - start_time,
                 )
 
