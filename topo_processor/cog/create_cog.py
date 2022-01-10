a25ac38ff8396cf6b5776fba3c7eff5646bc0b7f
Alice Fage <afage@linz.govt.nz>
2022-01-11 09:03:30 +1300

    fix: gdal_translate overview options

--- topo_processor/cog/create_cog.py
+++ topo_processor/cog/create_cog.py
@@ -5,7 +5,7 @@ from topo_processor.file_system.get_fs import bucket_name_from_path, is_s3_path
 from topo_processor.util.aws_credentials import Credentials, get_credentials
 
 
-def create_cog(input_path: str, output_path: str, compression_method: str) -> str:
+def create_cog(input_path: str, output_path: str, compression_method: str, overview_compression_method: str) -> str:
     cmd = Command("gdal_translate", {"container": "osgeo/gdal", "tag": "ubuntu-small-latest"})
     if is_s3_path(input_path):
         credentials: Credentials = get_credentials(bucket_name_from_path(input_path))
@@ -21,11 +21,11 @@ def create_cog(input_path: str, output_path: str, compression_method: str) -> st
     cmd.arg("-co", f"COMPRESS={compression_method}")
     cmd.arg("-co", "NUM_THREADS=ALL_CPUS")
     cmd.arg("-co", "PREDICTOR=2")
-    cmd.arg("-co", f"OVERVIEW_COMPRESS={compression_method}")
+    cmd.arg("-co", f"OVERVIEW_COMPRESS={overview_compression_method}")
     cmd.arg("-co", "BIGTIFF=YES")
     cmd.arg("-co", "OVERVIEW_RESAMPLING=LANCZOS")
     cmd.arg("-co", "BLOCKSIZE=512")
-    cmd.arg("-co", "QUALITY=90")
+    cmd.arg("-co", "OVERVIEW_QUALITY=90")
     cmd.arg("-co", "SPARSE_OK=TRUE")
     cmd.arg(output_path)
     return cmd
