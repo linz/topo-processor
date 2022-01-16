af6edb17491fffa1e2648dacd85adc80bfc9f501
Paul Fouquet <pfouquet@linz.govt.nz>
2022-01-17 10:59:25 +1300

    fix: add missing [type-arg] argument for bucket_roles

--- topo_processor/util/aws_credentials.py
+++ topo_processor/util/aws_credentials.py
@@ -21,7 +21,7 @@ class Credentials:
 
 session = boto3.Session(profile_name=os.getenv("AWS_PROFILE"))
 client: boto3.client = session.client("sts")
-bucket_roles: Dict[str, Any] = {}
+bucket_roles: Dict[str, Dict[str, Credentials]] = {}
 
 
 def get_credentials(bucket_name: str) -> Credentials:
