bedc3b481904c8eaddbe2c10c949b4959a68f671
Megan Davidson <mdavidson@linz.govt.nz>
2022-01-21 10:55:17 +1300

    fix: cast str

--- topo_processor/util/configuration.py
+++ topo_processor/util/configuration.py
@@ -1,11 +1,11 @@
 from os import environ
 from tempfile import mkdtemp
-from typing import Optional
+from typing import cast
 
 from dotenv import load_dotenv
 
 load_dotenv()
-lds_cache_bucket: str = environ.get("LDS_CACHE_BUCKET")
+lds_cache_bucket: str = cast(str, environ.get("LDS_CACHE_BUCKET"))
 aws_role_config_path: str = environ.get("AWS_ROLES_CONFIG")
-aws_profile: str = environ.get("AWS_PROFILE")
+aws_profile: str = cast(str, environ.get("AWS_PROFILE"))
 temp_folder: str = mkdtemp()
