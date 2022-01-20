9cfa4ce3ec0e3af4033b50c2a8b5cf64360a60c2
Paul Fouquet <pfouquet@linz.govt.nz>
2022-01-20 15:57:29 +1300

    fix: variable values type [type-var] (TDE-281)

--- topo_processor/util/configuration.py
+++ topo_processor/util/configuration.py
@@ -1,10 +1,10 @@
-from os import environ, path
+from os import environ
 from tempfile import mkdtemp
 
 from dotenv import load_dotenv
 
 load_dotenv()
 lds_cache_bucket: str = environ.get("LDS_CACHE_BUCKET")
-aws_role_config_path: str = path.expanduser(environ.get("AWS_ROLES_CONFIG"))
+aws_role_config_path: str = environ.get("AWS_ROLES_CONFIG")
 aws_profile: str = environ.get("AWS_PROFILE")
 temp_folder: str = mkdtemp()
