ca65f822d7f691aa927b291b61b6d318fc97442c
Megan Davidson <mdavidson@linz.govt.nz>
2022-01-21 10:57:35 +1300

    fix: rebase error

--- topo_processor/util/configuration.py
+++ topo_processor/util/configuration.py
@@ -6,6 +6,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 lds_cache_bucket: str = cast(str, environ.get("LDS_CACHE_BUCKET"))
-aws_role_config_path: str = environ.get("AWS_ROLES_CONFIG")
+aws_role_config_path: str = cast(str, environ.get("AWS_ROLES_CONFIG"))
 aws_profile: str = cast(str, environ.get("AWS_PROFILE"))
 temp_folder: str = mkdtemp()
