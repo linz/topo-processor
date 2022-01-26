552e6054a7cc66de417ff0f8f581a803900b725d
Megan Davidson <mdavidson@linz.govt.nz>
2022-01-26 14:17:31 +1300

    WIP

--- src/infra/index.ts
+++ src/infra/index.ts
@@ -4,5 +4,5 @@ import { AwsBatchStack } from './batch';
 const app = new App();
 new AwsBatchStack(app, 'Batch', {
   env: { region: 'ap-southeast-2', account: process.env['CDK_DEFAULT_ACCOUNT'] },
-  container: '..',
+  container: './batch',
 });
