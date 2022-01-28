51ca5da6daa9796005ff86d4ee8ab320e9a4794d
paulfouquet <86932794+paulfouquet@users.noreply.github.com>
2022-01-28 13:02:59 +1300

    fix: typing annotation None to Generator for pytest setup functions (TDE-285) (#525)
    * fix: change typing annotation None to Generator for pytest setup functions (TDE-285)
    
    * tests: refactoring and cleaning

--- conftest.py
+++ conftest.py
@@ -1,3 +1,7 @@
+import shutil
+from tempfile import mkdtemp
+from typing import Generator
+
 import pytest
 
 
@@ -8,3 +12,15 @@ def pytest_addoption(parser) -> None:  # type: ignore
 def pytest_runtest_setup(item) -> None:  # type: ignore
     if "slow" in item.keywords and not item.config.getoption("--slow"):
         pytest.skip("need --slow option to run this test")
+
+
+@pytest.fixture(autouse=True)
+def setup() -> Generator[str, None, None]:
+    """
+    This function creates a temporary directory and deletes it after each test.
+    See following link for details:
+    https://docs.pytest.org/en/stable/fixture.html#yield-fixtures-recommended
+    """
+    target = mkdtemp()
+    yield target
+    shutil.rmtree(target)
--- topo_processor/file_system/tests/write_json_test.py
+++ topo_processor/file_system/tests/write_json_test.py
@@ -1,25 +1,11 @@
 import os
-import shutil
-from tempfile import mkdtemp
 
 import pytest
 
 from topo_processor.file_system.write_json import write_json
 
 
-@pytest.fixture(autouse=True)
-def setup() -> None:
-    """
-    This function creates a temporary directory and deletes it after each test.
-    See following link for details:
-    https://docs.pytest.org/en/stable/fixture.html#yield-fixtures-recommended
-    """
-    target = mkdtemp()
-    yield target
-    shutil.rmtree(target)
-
-
-def test_write_json(setup) -> None:  # type: ignore
+def test_write_json(setup: str) -> None:
     my_dict = {"foo": "foo", "bar": 1}
     target = setup + "/test.json"
     write_json(my_dict, target)
