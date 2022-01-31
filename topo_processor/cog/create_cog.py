5d57921a1c99addbd3db545296930af232777b4a
Paul Fouquet <pfouquet@linz.govt.nz>
2022-01-31 15:42:48 +1300

    fix: issue when path was not a s3 link

--- topo_processor/cog/create_cog.py
+++ topo_processor/cog/create_cog.py
@@ -8,18 +8,19 @@ from topo_processor.util.s3 import bucket_name_from_path, is_s3_path
 
 
 def create_cog(input_path: str, output_path: str) -> Command:
-    if is_s3_path(input_path):
+    is_s3 = is_s3_path(input_path)
+    if is_s3:
         credentials: Credentials = get_credentials(bucket_name_from_path(input_path))
         input_path = f"/vsis3/{input_path.replace('s3://', '')}"
     if os.environ.get("IS_DOCKER") == "true":
         cmd = Command("gdal_translate")
-        if credentials:
+        if is_s3:
             os.environ["AWS_ACCESS_KEY_ID"] = credentials.access_key
             os.environ["AWS_SECRET_ACCESS_KEY"] = credentials.secret_key
             os.environ["AWS_SESSION_TOKEN"] = credentials.token
     else:
         cmd = Command("gdal_translate", {"container": "osgeo/gdal", "tag": "ubuntu-small-latest"})
-        if credentials:
+        if is_s3:
             cmd.env(f"AWS_ACCESS_KEY_ID={credentials.access_key}")
             cmd.env(f"AWS_SECRET_ACCESS_KEY={credentials.secret_key}")
             cmd.env(f"AWS_SESSION_TOKEN={credentials.token}")
