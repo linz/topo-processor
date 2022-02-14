1d8940deae7b167f8e01eaea1192e5301005b4ba
Alice Fage <afage@linz.govt.nz>
2022-02-14 13:33:18 +1300

    fix: raise exceptions on missing environment variable

--- topo_processor/util/configuration.py
+++ topo_processor/util/configuration.py
@@ -10,6 +10,4 @@ if lds_cache_bucket is None:
     raise Exception("$LDS_CACHE_BUCKET is not set")
 linz_ssm_bucket_config_name = environ.get("LINZ_SSM_BUCKET_CONFIG_NAME")
 aws_profile: Optional[str] = environ.get("AWS_PROFILE")
-if aws_profile is None:
-    raise Exception("$AWS_PROFILE is not set")
 temp_folder: str = mkdtemp()
