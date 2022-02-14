96c028ec1446e1039c9ff17d473458e6019d155b
Alice Fage <afage@linz.govt.nz>
2022-02-14 13:14:00 +1300

    fix: raise exceptions on missing environment variables

--- topo_processor/util/configuration.py
+++ topo_processor/util/configuration.py
@@ -1,11 +1,15 @@
 from os import environ
 from tempfile import mkdtemp
-from typing import cast
+from typing import Optional, cast
 
 from dotenv import load_dotenv
 
 load_dotenv()
-lds_cache_bucket: str = cast(str, environ.get("LDS_CACHE_BUCKET"))
+lds_cache_bucket: Optional[str] = environ.get("LDS_CACHE_BUCKET")
+if lds_cache_bucket is None:
+    raise Exception("$LDS_CACHE_BUCKET is not set")
 linz_ssm_bucket_config_name = environ.get("LINZ_SSM_BUCKET_CONFIG_NAME")
-aws_profile: str = cast(str, environ.get("AWS_PROFILE"))
+aws_profile: Optional[str] = environ.get("AWS_PROFILE")
+if aws_profile is None:
+    raise Exception("$AWS_PROFILE is not set")
 temp_folder: str = mkdtemp()
