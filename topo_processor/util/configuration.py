a9a23c466b76f2024c1232d39db65910d17133af
Blayne Chard <bchard@linz.govt.nz>
2022-02-15 13:09:29 +1300

    fix: ensure env vars actually exist

--- topo_processor/util/configuration.py
+++ topo_processor/util/configuration.py
@@ -5,7 +5,14 @@ from typing import cast
 from dotenv import load_dotenv
 
 load_dotenv()
-lds_cache_bucket: str = cast(str, environ.get("LDS_CACHE_BUCKET"))
+
+def get_env(env_name:str) -> str:
+    env_var = environ.get(env_name)
+    if env_var is None:
+        raise Exception(f"Missing environment variable ${env_name}")
+    return env_var
+
+lds_cache_bucket: str = get_env("LINZ_CACHE_BUCKET")
+aws_profile: str = get_env("AWS_PROFILE")
 linz_ssm_bucket_config_name = environ.get("LINZ_SSM_BUCKET_CONFIG_NAME")
-aws_profile: str = cast(str, environ.get("AWS_PROFILE"))
 temp_folder: str = mkdtemp()
