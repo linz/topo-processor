426c9eac043d9b546ed3f2e085ccbe709f268fbd
Blayne Chard <bchard@linz.govt.nz>
2022-02-15 13:38:45 +1300

    fix: AWS_PROFILE is not required it is optional

--- topo_processor/util/configuration.py
+++ topo_processor/util/configuration.py
@@ -1,12 +1,12 @@
 from os import environ
 from tempfile import mkdtemp
-from typing import cast
+from typing import Optional
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-
+# Hello World
 def get_env(env_name: str) -> str:
     env_var = environ.get(env_name)
     if env_var is None:
@@ -15,6 +15,6 @@ def get_env(env_name: str) -> str:
 
 
 lds_cache_bucket: str = get_env("LINZ_CACHE_BUCKET")
-aws_profile: str = get_env("AWS_PROFILE")
-linz_ssm_bucket_config_name = environ.get("LINZ_SSM_BUCKET_CONFIG_NAME")
+aws_profile: Optional[str] = environ.get("AWS_PROFILE")
+linz_ssm_bucket_config_name: Optional[str] = environ.get("LINZ_SSM_BUCKET_CONFIG_NAME")
 temp_folder: str = mkdtemp()
