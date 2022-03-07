550826420ceace64645a8f759859ec798673108f
Alice Fage <afage@linz.govt.nz>
2022-03-08 12:05:46 +1300

    fix: formatting, mypy

--- topo_processor/util/s3.py
+++ topo_processor/util/s3.py
@@ -1,9 +1,5 @@
-
 import boto3
-
 from linz_logger import get_log
-
-
 
 
 def bucket_name_from_path(path: str) -> str:
@@ -14,18 +10,21 @@ def bucket_name_from_path(path: str) -> str:
 def bucket_name_from_stack(stack_name: str) -> str:
     get_log().debug("stack_name", stack_name=stack_name)
     session = boto3.Session()
-    cloudformation = session.resource('cloudformation')
+    cloudformation = session.resource("cloudformation")
     stack = cloudformation.Stack(stack_name)
+
+    temp_bucket: str = ""
 
     try:
         for output in stack.outputs:
             if output["OutputKey"] == "TempBucketName":
                 get_log().debug("bucket_name", bucket_name=output["OutputValue"])
-                return output["OutputValue"]
+                temp_bucket = output["OutputValue"]
     except Exception as e:
         get_log().error("bucket_name_not_found", stackName=stack_name, error=e)
         raise e
 
+    return temp_bucket
 
 
 def is_s3_path(path: str) -> bool:
