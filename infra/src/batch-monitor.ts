41a6102772ccf1a29b7fbb3fc0004a107086e742
Megan Davidson <mdavidson@linz.govt.nz>
2022-03-28 15:43:10 +1300

    feat(cdk): lambda function to monitor batch job failures
    using event bridge

---
+++ infra/src/batch-monitor.ts
@@ -0,0 +1,29 @@
+import { Stack, StackProps } from 'aws-cdk-lib';
+import * as events from 'aws-cdk-lib/aws-events';
+import * as evtTargets from 'aws-cdk-lib/aws-events-targets';
+import * as lf from 'aws-cdk-lib/aws-lambda';
+import { Code } from 'aws-cdk-lib/aws-lambda';
+import { Construct } from 'constructs';
+import * as path from 'path';
+
+export class AwsBatchMonitor extends Stack {
+  public constructor(scope: Construct, id: string, props: StackProps) {
+    super(scope, id, props);
+
+    const rule = new events.Rule(this, 'BatchEventRule', {
+      eventPattern: {
+        source: ['aws.batch'],
+        detailType: ['Batch Job State Change'],
+        detail: { status: ['FAILED'] },
+      },
+    });
+
+    const lambda = new lf.Function(this, 'BatchLog', {
+      runtime: lf.Runtime.NODEJS_14_X,
+      handler: 'index.handler',
+      code: Code.fromAsset(path.join(process.cwd(), 'infra', 'src', 'lambda-code', 'index.js')),
+    });
+
+    rule.addTarget(new evtTargets.LambdaFunction(lambda));
+  }
+}
--- infra/src/index.ts
+++ infra/src/index.ts
@@ -1,5 +1,6 @@
 import { App } from 'aws-cdk-lib';
 import { AwsBatchStack } from './batch';
+import { AwsBatchMonitor } from './batch-monitor';
 
 const app = new App();
 new AwsBatchStack(app, 'TopoProcessorBatch', {
@@ -8,4 +9,10 @@ new AwsBatchStack(app, 'TopoProcessorBatch', {
     account: process.env['CDK_DEFAULT_ACCOUNT'],
   },
   container: './',
+});
+new AwsBatchMonitor(app, 'TopoProcessorBatchMon', {
+  env: {
+    region: 'ap-southeast-2',
+    account: process.env['CDK_DEFAULT_ACCOUNT'],
+  },
 });
