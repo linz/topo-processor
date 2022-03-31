484d5f12ed1c4b1a658aedc9bb6875207bc48152
Alice Fage <afage@linz.govt.nz>
2022-03-31 13:49:33 +1300

    chore: bump stac versions

--- topo_processor/stac/stac_extensions.py
+++ topo_processor/stac/stac_extensions.py
@@ -2,13 +2,13 @@ from enum import Enum
 
 
 class StacExtensions(str, Enum):
-    linz = "https://stac.linz.govt.nz/v0.0.14/linz/schema.json"
-    quality = "https://stac.linz.govt.nz/v0.0.14/quality/schema.json"
-    historical_imagery = "https://stac.linz.govt.nz/v0.0.14/historical-imagery/schema.json"
-    aerial_photo = "https://stac.linz.govt.nz/v0.0.14/aerial-photo/schema.json"
-    camera = "https://stac.linz.govt.nz/v0.0.14/camera/schema.json"
-    film = "https://stac.linz.govt.nz/v0.0.14/film/schema.json"
-    scanning = "https://stac.linz.govt.nz/v0.0.14/scanning/schema.json"
+    linz = "https://stac.linz.govt.nz/v0.0.15/linz/schema.json"
+    quality = "https://stac.linz.govt.nz/v0.0.15/quality/schema.json"
+    historical_imagery = "https://stac.linz.govt.nz/v0.0.15/historical-imagery/schema.json"
+    aerial_photo = "https://stac.linz.govt.nz/v0.0.15/aerial-photo/schema.json"
+    camera = "https://stac.linz.govt.nz/v0.0.15/camera/schema.json"
+    film = "https://stac.linz.govt.nz/v0.0.15/film/schema.json"
+    scanning = "https://stac.linz.govt.nz/v0.0.15/scanning/schema.json"
     eo = "https://stac-extensions.github.io/eo/v1.0.0/schema.json"
     file = "https://stac-extensions.github.io/file/v2.0.0/schema.json"
     projection = "https://stac-extensions.github.io/projection/v1.0.0/schema.json"
