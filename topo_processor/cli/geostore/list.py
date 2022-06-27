02897ff3cfebe32381f1e22c591bd2270f4a4c21
Alice Fage <afage@linz.govt.nz>
2022-06-28 11:32:15 +1200

    fix: change survey to title

--- topo_processor/cli/geostore/list.py
+++ topo_processor/cli/geostore/list.py
@@ -8,7 +8,7 @@ from topo_processor.util.time import time_in_ms
 @click.command()
 @click.option(
     "-s",
-    "--survey",
+    "--title",
     required=False,
     help="The Geostore title of the survey to filter",
 )
@@ -18,18 +18,18 @@ from topo_processor.util.time import time_in_ms
     is_flag=True,
     help="Use verbose to display debug logs",
 )
-def main(survey: str, verbose: bool) -> None:
+def main(title: str, verbose: bool) -> None:
     start_time = time_in_ms()
     logger = get_log()
-    logger.info("list_datasets_start", survey=survey)
+    logger.info("list_datasets_start", title=title)
 
     if not verbose:
         set_level(LogLevel.info)
 
     try:
         list_parameters = {}
-        if survey:
-            list_parameters = {"title": survey}
+        if title:
+            list_parameters = {"title": title}
         dataset_list = invoke_lambda("datasets", "GET", list_parameters)
 
         logger.info("list_datasets_end", dataset_list=dataset_list, duration=time_in_ms() - start_time)
