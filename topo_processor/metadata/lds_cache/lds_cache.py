fcdb099a72771657ae11d3fbb3656fe5ffe8fc76
Alice Fage <afage@linz.govt.nz>
2022-07-27 11:26:37 +1200

    fix: remove unused csv gzip code

--- topo_processor/metadata/lds_cache/lds_cache.py
+++ topo_processor/metadata/lds_cache/lds_cache.py
@@ -54,7 +54,7 @@ def get_metadata(
 
             if os.path.isfile(metadata_path):
                 if exported_asset.extra_fields.get("encoding", None) == "gzip":
-                    decompress_file(metadata_path, True)
+                    decompress_file(metadata_path)
                     new_metadata_path = os.path.splitext(metadata_path)[0] + "_" + time.strftime("%s") + ".csv"
                     geopackage_to_csv(metadata_path, new_metadata_path).run()
                     metadata_path = new_metadata_path
--- topo_processor/util/gzip.py
+++ topo_processor/util/gzip.py
@@ -11,15 +11,12 @@ def is_gzip_file(file_path: str) -> bool:
         return file.read(2) == b"\x1f\x8b"
 
 
-def decompress_file(file_path: str, is_binary: bool) -> None:
+def decompress_file(file_path: str) -> None:
     input: Optional[gzip.GzipFile] = None
 
     try:
         input = gzip.GzipFile(file_path, "rb")
-        if is_binary:
-            input_read = input.read()
-        else:
-            input_read = input.read().decode("utf-8-sig")  # type: ignore[assignment]
+        input_read = input.read()
     except gzip.BadGzipFile as e:
         get_log().error("File decompression failed", file=file_path, error=e)
         raise e
@@ -28,10 +25,7 @@ def decompress_file(file_path: str, is_binary: bool) -> None:
         if input:
             input.close()
 
-    if is_binary:
-        output = open(file_path, "wb")
-    else:
-        output = open(file_path, "w")  # type: ignore[assignment]
+    output = open(file_path, "wb")
 
     output.write(input_read)
     output.close()
